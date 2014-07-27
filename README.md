79f5352316cdb3d00bb360da42b9c18231b6c157
bin.yin <Bin.Yin@citrix.com>
2014-07-27 08:06:07 -0400

    version 1.0 Beta

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ Written by Python(2.7.6)
 
 ##### 1. *.xib to *.strings
 ```
-ibtool.py --generate-strings-file SFConfigWizardViewController.xib.strings SFConfigWizardViewController.xib
+ibtool.py --generate-strings-file destFile.xib.strings resFile.xib
 ```
-(Extract the UI strings at 'SFConfigWizardViewController.xib' to 'SFConfigWizardViewController.xib.strings'.)
+Extract the UI strings at 'resFile.xib' to 'destFile.xib.strings'.
+
+##### 2. *.strings to *.xib
+```
+ibtool.py --strings-file translated.xib.strings  --write en\enResFile.xib destL10nFile.xib  
+```
+Create a localized file with the translation at 'translated.xib.strings' and template at 'en\enResFile.xib'
+
+Attention: this tool cannot replace Mac ibtool since it only extract the hard code strings marked with "NSContents" up to now.
--- ibtool.py
+++ ibtool.py
@@ -1,6 +1,8 @@
 import xml.etree.ElementTree as ET
 import sys
 import os
+import codecs
+
 
 __author__ = 'Bin YIN'
 __email__ = 'ybdesire@gmail.com'
@@ -9,9 +11,9 @@ def parsePara():
     if sys.argv[1]=="--generate-strings-file":
         xibToStrings(sys.argv[2], sys.argv[3])
     elif sys.argv[1]=="--strings-file" and sys.argv[3]=="--write":
-        print "not support --strings-file now"
+        stringsToXib(sys.argv[2], sys.argv[4], sys.argv[5])
     else:
-        print "wrong useage"
+        print("wrong useage")
 
 def xibToStrings(stringsFileDestPath, xibSourcePath):
     #create a file with encoding "UTF8 without BOM" for support Mac *.strings file format
@@ -30,8 +32,36 @@ def xibToStrings(stringsFileDestPath, xibSourcePath):
         if value == "NSContents":
             if string.text is not None:
                 stringsFile.write('\"' + string.text +'\"' + '=' + '\"' + string.text + '\"' + ';\r');#'\r' for Mac
+    stringsFile.close()
+                
+def stringsToXib(stringsFileSrcPath, enXibFileSrcPath, l10nXibFileDestPath):
+    l10nXibFilePath = os.path.split(l10nXibFileDestPath)[0]
+    l10nXibFileName = os.path.split(l10nXibFileDestPath)[1]
+    if l10nXibFilePath != "":#if not at current working directory
+        if not os.path.exists(l10nXibFilePath):#if path not exists
+            os.makedirs(l10nXibFilePath)
+    l10nXibFile = open(l10nXibFileDestPath,'w', encoding="utf-8")
+    enXibFile = open(enXibFileSrcPath, 'r', encoding="utf-8")
+    #parse the *.strings file and creat an dictionary
+    stringsFile = open(stringsFileSrcPath, 'rU', encoding="utf-8")#'rU' for mac end-of-line '\r'
+    stringsDict = dict()
+    for line in stringsFile:
+        key = line.split("=")[0].replace("\"","")
+        value = line.split("=")[1].replace("\"","").replace(";", "").rstrip('\n')
+        stringsDict[key] = value
+    
+    #parse the EN src xib file
+    tree = ET.parse(enXibFileSrcPath)
+    root = tree.getroot()
+    for string in root.iter("string"):
+        value = string.get("key")
+        if value == "NSContents":
+            if string.text is not None and string.text in stringsDict:
+                string.text = stringsDict[string.text]
+    tree.write(l10nXibFileDestPath, encoding="utf-8",xml_declaration=True)
+    l10nXibFile.close()           
 
-def test():
+def parseXibAndGetStrings():
     xibFile = sys.argv[1]
     tree = ET.parse(xibFile)
     root = tree.getroot()
@@ -39,8 +69,40 @@ def test():
         value = string.get("key")
         if value == "NSContents":
             if string.text is not None:
-                print string.text
+                print(string.text)
 
+def test():
+    stringsFile = open("test.txt", 'rU')
+    stringsFile.seek(0)
+    for line in stringsFile:
+        print(line)
+    stringsFile.close()
+
+def fileSupportUnicode():
+    #reload(sys)
+    #sys.setdefaultencoding( "utf-8" )
+
+    stringsFile = open("SFConfigWizardViewController.xib.strings", 'rU', encoding="utf-8")
+    stringsDict = dict()
+    for line in stringsFile:
+        print(line)
+        key = line.split("=")[0].replace("\"","")
+        value = line.split("=")[1].replace("\"","").replace(";", "").replace("\r", "")
+        print("key:value: " + key + ":" + value)
+        stringsDict[key]=value
+    destFile = open("testSC.xml", "w", encoding="utf-8")
+    destFile.write(stringsDict["Go Back"])
+    print(stringsDict["Go Back"])
+    whatisthis(stringsDict["Go Back"])
+
+def whatisthis(s):
+    if isinstance(s, str):
+        print("ordinary string")
+    elif isinstance(s, unicode):
+        print("unicode string")
+    else:
+        print("not a string")
+        
 if __name__=='__main__':
     parsePara()
 
